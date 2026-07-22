43117ad661516b52378defd676d5d743b111466f
praveensen-12107 <praveensen12107@gmail.com>
2026-07-22 12:36:58 +0530

    Time: 1 ms (60.32%), Space: 48.7 MB (16.06%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,26 +1,26 @@
 class Solution {
     public int trap(int[] height) {
         int left [] = new int [height.length];
-        int right [] = new int [height.length];
+        int right [] =new int [height.length];
         int maxl=height[0];
         int maxr=height[height.length-1];
-        int ans=0;
         for(int i=0;i<height.length;i++){
-            maxl = Math.max(maxl, height[i]);
-            left[i] = maxl;
-            
+           maxl = Math.max(maxl, height[i]);
+           left[i] = maxl;
         }
         for(int i=height.length-1;i>=0;i--){
             maxr=Math.max(maxr,height[i]);
             right[i]=maxr;
-            
+        
         }
+        int trap_water=0;
         for(int i=0;i<height.length;i++){
-            int minh=Math.min(left[i],right[i]);
-            ans+=minh-height[i];
-
+            int minh= Math.min(left[i],right[i]);
+            trap_water+=minh-height[i];
         }
-        return ans;
+        return trap_water;
+
+
         
     }
 }
