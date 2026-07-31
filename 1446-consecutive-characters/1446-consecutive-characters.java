9e0d1f274ea1cb610c5efcde55eddddb08b9d218
praveensen-12107 <praveensen12107@gmail.com>
2026-07-31 13:52:56 +0530

    Time: 1 ms (100%), Space: 43.7 MB (11.69%) - LeetHub

---
+++ 1446-consecutive-characters/1446-consecutive-characters.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxPower(String s) {
+        if(s.length()==0){
+            return 0;
+        }
+        int count=1;
+        int max=1;
+        for(int i=1;i<s.length();i++){
+            if(s.charAt(i)==s.charAt(i-1)){
+                count++;
+            }
+            else{
+                count=1;
+            }
+            max=Math.max(count,max);
+        }
+        return max;
+        
+    }
+}
