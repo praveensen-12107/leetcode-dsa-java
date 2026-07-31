b0520cafdd90745984138216b8ad534e778ac695
praveensen-12107 <praveensen12107@gmail.com>
2026-07-31 16:32:27 +0530

    Time: 3 ms (86.07%), Space: 43 MB (86.19%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(ch=='(' || ch=='{' || ch=='['){
+                st.push(ch);
+            }
+            else{
+                if (st.isEmpty()) {
+                return false;
+            }
+                char top = st.peek();
+                if((ch==')' && top!='(')
+                 ||(ch==']' && top!='[' ) 
+                 ||(ch=='}' && top!='{')){
+                    return false;
+                 }
+                 st.pop();
+            }
+        }
+        if(st.size()==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+}
